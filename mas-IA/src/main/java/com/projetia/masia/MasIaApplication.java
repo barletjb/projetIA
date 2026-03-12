6be206684f044b2b495f088d9f496380dc025d67
Jean-Baptiste Barlet <barletjb.perso@gmail.com>
2026-03-12 10:57:06 +0100

    Initial commit Angular + Spring Boot

---
+++ mas-IA/src/main/java/com/projetia/masia/MasIaApplication.java
@@ -0,0 +1,13 @@
+package com.projetia.masia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasIaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasIaApplication.class, args);
+    }
+
+}
---
+++ mas-IA/src/test/java/com/projetia/masia/MasIaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetia.masia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasIaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
