78472e690de8175ec499372f9dc90f4da34401ae
barletjb <barletjb.perso@gmail.com>
2026-03-21 14:32:33 +0100

    change back

--- mas-IA/src/main/java/fr/eni/masia/MasIaApplication.java
+++ mas-IA/src/main/java/fr/eni/masia/MasIaApplication.java
@@ -1,4 +1,4 @@
-package com.projetia.masia;
+package fr.eni.masia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- mas-IA/src/test/java/fr/eni/masia/MasIaApplicationTests.java
+++ mas-IA/src/test/java/fr/eni/masia/MasIaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.projetia.masia;
+package fr.eni.masia;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
