c1c6b426c214ac97cf9d6278480db0fb5b523649
barletjb <barletjb.perso@gmail.com>
2026-04-24 11:06:53 +0200

    Add Groq DTO

---
+++ ai-gateway/src/main/java/fr/eni/aigateway/model/GroqRequestDTO.java
@@ -0,0 +1,24 @@
+package fr.eni.aigateway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class GroqRequestDTO {
+
+    private String model;
+    private List<Message> messages;
+
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    public static class Message {
+        private String role;
+        private String content;
+    }
+}
---
+++ ai-gateway/src/main/java/fr/eni/aigateway/model/GroqResponseDTO.java
@@ -0,0 +1,24 @@
+package fr.eni.aigateway.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class GroqResponseDTO {
+
+    private List<Choice> choices;
+
+    @Data
+    public static class Choice {
+        private Message message;
+    }
+
+    @Data
+    public static class Message {
+        private String content;
+    }
+}
