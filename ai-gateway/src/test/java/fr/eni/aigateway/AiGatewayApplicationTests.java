1a99569212aaf7e0eb0c824f07974edfc0470154
barletjb <barletjb.perso@gmail.com>
2026-04-24 10:09:50 +0200

    New module API IA

---
+++ ai-gateway/src/test/java/fr/eni/aigateway/AiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.aigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
