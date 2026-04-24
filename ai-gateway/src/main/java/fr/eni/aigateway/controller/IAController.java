af3ef64433e1e19dfeca4cb02b2ebe9d952e0545
barletjb <barletjb.perso@gmail.com>
2026-04-24 11:07:29 +0200

    Add IA controller et validation

--- ai-gateway/src/main/java/fr/eni/aigateway/controller/IAController.java
+++ ai-gateway/src/main/java/fr/eni/aigateway/controller/IAController.java
@@ -1,4 +1,25 @@
 package fr.eni.aigateway.controller;
 
+import fr.eni.aigateway.model.IARequestDTO;
+import fr.eni.aigateway.model.IAResponseDTO;
+import fr.eni.aigateway.service.GroqService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/ia")
 public class IAController {
+
+    private final GroqService groqService;
+
+    @PostMapping("/groq")
+    @ResponseStatus(HttpStatus.OK)
+    public IAResponseDTO groqRequest(@Valid @RequestBody IARequestDTO requestDTO) {
+
+        return groqService.sendPrompt(requestDTO);
+    }
+
 }
