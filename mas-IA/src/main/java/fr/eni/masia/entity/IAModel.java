9beb057c365eec87d5dfc6b91e56a887a00321c4
barletjb <barletjb.perso@gmail.com>
2026-04-26 10:22:35 +0200

    Add Entity pour IA

---
+++ mas-IA/src/main/java/fr/eni/masia/entity/IAModel.java
@@ -0,0 +1,30 @@
+package fr.eni.masia.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "IA_MODEL")
+public class IAModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String modelId;
+
+    @Column(nullable = false)
+    private String provider;
+
+    @Column(nullable = false)
+    private Boolean active;
+}
