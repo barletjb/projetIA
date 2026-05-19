1a83a05d107718064c20bbde98e531e9fd1900b5
barletjb <barletjb.perso@gmail.com>
2026-05-19 08:52:46 +0200

    Appel API Front + CORS

---
+++ prompt-hub-frontend/src/app/prompts/prompt-service.ts
@@ -0,0 +1,19 @@
+import {Injectable, inject} from '@angular/core'
+import {Prompt} from './models/prompt.models';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PromptService {
+
+  httpCLient = inject(HttpClient)
+  apiUrl = environment.apiUrl
+
+  getPrompts(): Observable<Prompt[]> {
+    return this.httpCLient.get<Prompt[]>(this.apiUrl + 'prompts')
+
+  }
+}
