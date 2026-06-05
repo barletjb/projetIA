83ce8d8a9949b1ae56e06a01ac2b4e6abbea91fc
barletjb <barletjb.perso@gmail.com>
2026-06-05 23:56:33 +0200

    Add Register form

--- prompt-hub-frontend/src/app/components/register/register.ts
+++ prompt-hub-frontend/src/app/components/register/register.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core'
+import {Component, inject} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {Router, RouterLink} from '@angular/router';
+import { passwordMatchValidator } from '../../shared/validators/passwordMatchValidator'
+import {Card} from 'primeng/card';
+import {Button} from 'primeng/button';
+
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, Card, Button, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.scss',
 })
-export class Register {}
+export class Register {
+
+  private readonly fb = inject(FormBuilder)
+  private readonly router = inject(Router)
+
+  registerForm = this.fb.group({
+    fullName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+(?: [a-zA-Z]+)*$/)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+    confirmPassword: ['', Validators.required]
+  }, {
+    validators: passwordMatchValidator
+  })
+
+  get fullName() {
+    return this.registerForm.controls['fullName'];
+  }
+
+  get email() {
+    return this.registerForm.controls['email'];
+  }
+
+  get password() {
+    return this.registerForm.controls['password'];
+  }
+
+  get confirmPassword() {
+    return this.registerForm.controls['confirmPassword'];
+  }
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log('Formulaire valide:', this.registerForm.value);
+    } else {
+      console.log('Formulaire invalide');
+      this.markFormGroupTouched();
+    }
+  }
+
+  private markFormGroupTouched(): void {
+    Object.keys(this.registerForm.controls).forEach((key) => {
+      const control = this.registerForm.get(key);
+      control?.markAsTouched();
+    });
+  }
+
+
+}
